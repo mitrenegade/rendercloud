70856c797f7c7cc27a3e782187eb51f95acbc42b
bobbyren <bobbyren@gmail.com>
2019-04-02 00:59:21 -0400

    refactor to have doLoadCloud and doLoadDatabase

--- Example/RenderCloud/DemoViewController.swift
+++ Example/RenderCloud/DemoViewController.swift
@@ -15,9 +15,12 @@ class DemoViewController: UIViewController {
     @IBOutlet weak var toggle: UISwitch!
     @IBOutlet weak var labelToggle: UILabel!
     
-    @IBOutlet weak var button: UIButton!
-    @IBOutlet weak var labelButton: UILabel!
-    
+    @IBOutlet weak var buttonCloud: UIButton!
+    @IBOutlet weak var labelCloud: UILabel!
+
+    @IBOutlet weak var buttonRef: UIButton!
+    @IBOutlet weak var labelRef: UILabel!
+
     var apiService: CloudAPIService?
     var ref: Reference?
     var snapshot: Snapshot?
@@ -30,15 +33,8 @@ class DemoViewController: UIViewController {
         toggle.isOn = AIRPLANE_MODE
         labelToggle.text = AIRPLANE_MODE ? "AIRPLANE_MODE" : "CONNECTED"
         
-        labelButton.text = "Click to update"
-    }
-    
-    @IBAction func didClickButton(_ sender: Any?) {
-        if AIRPLANE_MODE {
-            loadAirplaneMode()
-        } else {
-            loadConnected()
-        }
+        labelCloud.text = "Click to load from cloud"
+        labelCloud.text = "Click to load from database"
     }
     
     @IBAction func didToggle(_ sender: UISwitch) {
@@ -46,22 +42,25 @@ class DemoViewController: UIViewController {
         labelToggle.text = AIRPLANE_MODE ? "AIRPLANE_MODE" : "CONNECTED"
     }
     
-    func loadConnected() {
-        apiService = FirebaseAPIService()
-//        ref = apiService.child
-        
-        doLoad()
+    @IBAction func didClickButton(_ sender: UIButton) {
+        // setup data
+        if AIRPLANE_MODE {
+            apiService = MockCloudAPIService(uniqueId: "123", results: ["data": "abc"])
+            snapshot = MockDataSnapshot(exists: true, value: ["data": "123"])
+            ref = MockDatabaseReference(snapshot: snapshot!)
+        } else {
+            apiService = FirebaseAPIService()
+        }
+
+        // make protocol request
+        if sender == buttonCloud {
+            doLoadCloud()
+        } else {
+            doLoadRef()
+        }
     }
     
-    func loadAirplaneMode() {
-        apiService = MockCloudAPIService(uniqueId: "123", results: ["data": "abc"])
-        snapshot = MockDataSnapshot(exists: true, value: ["data": "123"])
-        ref = MockDatabaseReference(snapshot: snapshot!)
-        
-        doLoad()
-    }
-    
-    private func doLoad() {
+    private func doLoadCloud() {
         // CloudAPIService
         apiService?.getUniqueId { [weak self] (id) in
             guard let id = id else {
@@ -73,12 +72,16 @@ class DemoViewController: UIViewController {
                 print("Result \(String(describing: result)) error \(String(describing: error))")
                 DispatchQueue.main.async { [weak self] in
                     if let result = result {
-                        self?.labelButton.text = "\(result)"
+                        self?.labelCloud.text = "\(result)"
                     } else if let error = error as NSError? {
-                        self?.labelButton.text = error.debugDescription
+                        self?.labelCloud.text = error.debugDescription
                     }
                 }
             }
         }
+    }
+    
+    private func doLoadRef() {
+        
     }
 }
