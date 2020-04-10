799f02bfa3ed9ab00945c6433e91de747d980b5b
Bobby Ren <bobbyren@gmail.com>
2020-04-10 12:21:23 -0700

    added CloudDatabaseService file; added reference(with child)

---
+++ RenderCloud/Classes/CloudDatabaseProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  CloudDatabaseProtocol.swift
+//  RenderCloud
+//
+//  Created by Bobby Ren on 4/10/20.
+//
+
+// interface into specific uses of the Database protocol
+public protocol CloudDatabaseService {
+    func connectedAccount(with userId: String) -> Reference?
+    func reference(with child: String) -> Reference?
+}
