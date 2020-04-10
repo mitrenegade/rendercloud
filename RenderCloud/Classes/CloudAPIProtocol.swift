c0759e9e7a1108a0a5f0cd9c979951370083374d
Bobby Ren <bobbyren@gmail.com>
2020-04-10 08:02:04 -0700

    added CloudDatabaseService; connectedAccount is implemented in RenderAPIService

--- RenderCloud/Classes/CloudAPIProtocol.swift
+++ RenderCloud/Classes/CloudAPIProtocol.swift
@@ -7,10 +7,16 @@
 
 import Foundation
 
+// interface into using cloud functions
 public protocol CloudAPIService {
     func cloudFunction(functionName: String, completion: ((_ response: Any?, _ error: Error?) -> ())?)
     func cloudFunction(functionName: String, method: String, params: [String: Any]?, completion: ((_ response: Any?, _ error: Error?) -> ())?)
     func getUniqueId(completion: @escaping ((String?)->()))
     func uniqueId() -> String
     var baseUrl: URL? { get }
+}
+
+// interface into specific uses of the Database protocol
+public protocol CloudDatabaseService {
+    func connectedAccount(with userId: String) -> Reference?
 }
